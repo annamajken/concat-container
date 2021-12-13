6851aca4c2274cbf315948ee6f9bb125f2e3ab90
Anna Bergvall <anna.v.bergvall@gmail.com>
2021-12-13 11:14:48 +0100

    Update join_iterator.h

--- join_iterator.h
+++ join_iterator.h
@@ -4,7 +4,12 @@
 template <typename T> class join_iterator {
     public:
         typedef T value_type;
-    
+        typedef typename value_type::iterator c1_begin;
+        typedef typename value_type::iterator c1_end;
+        typedef typename value_type::iterator c2_begin;
+        typedef typename value_type::iterator c2_end;
+        join_iterator(c1_begin, c1_end, c2_begin, c2_end);
+    private:
 }
 
 #endif
