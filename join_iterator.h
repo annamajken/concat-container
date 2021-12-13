07ca90c11c5ceb48483d61a1e38abb1b4f422e9b
Anna Bergvall <anna.v.bergvall@gmail.com>
2021-12-13 11:23:50 +0100

    Update join_iterator.h

--- join_iterator.h
+++ join_iterator.h
@@ -1,6 +1,8 @@
 #ifndef JOIN_ITERATOR_H
 #define JOIN_ITERATOR_H
 
+
+
 template <typename T> class join_iterator {
     public:
         typedef T value_type;
@@ -8,7 +10,10 @@ template <typename T> class join_iterator {
         typedef typename value_type::iterator c1_end;
         typedef typename value_type::iterator c2_begin;
         typedef typename value_type::iterator c2_end;
+        // eller ska man bara ta in två collections istället
+        // då kan man ju få tag på iteratorerna i denna klassen
         join_iterator(c1_begin, c1_end, c2_begin, c2_end);
+
     private:
 }
 
